c950b3af32afcb5b77f32d892fa15f570bbd04aa
devpuskar <devpuskar@gmail.com>
2023-02-05 13:31:08 -0600

    Begin Functional programming

---
+++ src/EX01IntroFunctional.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class EX01IntroFunctional {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        printSquaresOfEvenNumbersInListFunctional(numbers);
+    }
+
+    private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+        // What to do?
+        numbers.stream().forEach(System.out::println);// Method Reference
+    }
+
+    // number -> number%2 == 0
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream() // Convert to Stream
+                .filter(number -> number % 2 == 0) // Lamdba Expression
+                .forEach(System.out::println);// Method Reference
+
+        // .filter(FP01Functional::isEven)//Filter - Only Allow Even Numbers
+    }
+
+    private static void printSquaresOfEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream() // Convert to Stream
+                .filter(number -> number % 2 == 0) // Lamdba Expression
+                //mapping - x -> x * x
+                .map(number -> number * number)
+                .forEach(System.out::println);// Method Reference
+
+    }
+}
+
